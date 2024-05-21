ce1fb668c3d74548ebc9f149a1a8e2426c0e655d
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2024-05-21 12:47:03 +0300

    add suspense boundary

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -4,21 +4,28 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function NavbarItem({ title, param }) {
+function Item({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
+
+  return (
+    <Link
+      className={`hover:text-amber-600 font-semibold ${
+        genre === param
+          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+          : ""
+      }`}
+      href={`/?genre=${param}`}
+    >
+      {title}
+    </Link>
+  );
+}
+
+export default function NavbarItem({ title, param }) {
   return (
     <Suspense>
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
+      <Item title={title} param={param} />
     </Suspense>
   );
 }
